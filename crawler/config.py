542b55ac2058b12412712625909a4eff9ba7ac39
abantikaChail <69887436+abantikaChail@users.noreply.github.com>
2025-04-14 18:29:10 +0530

    Update config.py

--- crawler/config.py
+++ crawler/config.py
@@ -1,10 +1,13 @@
+# config.py
 PRODUCT_PATTERNS = [
-    "/product/",
-    "/p/",
-    "/item/",
-    "/prod/",
-    "/shop/"
+    r"/product[s]?/[a-zA-Z0-9\-_]+",
+    r"/p/[a-zA-Z0-9\-_]+",
+    r"/item/[a-zA-Z0-9\-_]+",
+    r"/prod/[a-zA-Z0-9\-_]+",
+    r"/shop/[a-zA-Z0-9\-_]+",
+    r"/[a-zA-Z0-9\-_]+/p$"
 ]
 
+
 MAX_CONCURRENCY = 10
-MAX_DEPTH = 3
+MAX_DEPTH = 6
