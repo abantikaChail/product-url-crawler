03a1cbb6d49c07f757e28971d54671ef7cc69601
abantikaChail <69887436+abantikaChail@users.noreply.github.com>
2025-04-14 18:39:30 +0530

    Update safe_runner.py

--- safe_runner.py
+++ safe_runner.py
@@ -1,1 +1,8 @@
+import asyncio
+import sys
+
+def run_main(main_coro):
+    if sys.platform.startswith("win"):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(main_coro)
 
